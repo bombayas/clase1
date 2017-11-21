8678117c50d46ac8195be041764378463fd84722
bombayas <33655524+bombayas@users.noreply.github.com>
2017-11-21 11:53:33 +0100

    Add files via upload

---
+++ pyton informatica/nif.py
@@ -0,0 +1,7 @@
+def nifletra():
+    dni=input("dime tu dni")
+    letras=("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E")
+    resto=dni%23
+    print letras[resto]
+nifletra()
+    
