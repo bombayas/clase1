9a427c58194ca078ba7767814959b053a0f6cf16
bombayas <33655524+bombayas@users.noreply.github.com>
2017-11-24 12:59:01 +0100

    Add files via upload

---
+++ suma parecida vectores.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+
+#include<iostream>
+int main(){
+    int vector[5];
+    int cont;
+    int suma=0;
+    int salir;
+    for (cont=0;cont<5;cont++){
+        cout<<"dime un nº";
+        cin>>vector[cont];
+        }
+        cout<< "la suma vale:";
+        for(cont=0;cont<5;cont++){
+           suma=suma+vector[cont];
+}
+           cout<<suma;
+
+           cout<<"toca c.tecla";
+           cin>>salir;
+           return 0;
+             
+}
