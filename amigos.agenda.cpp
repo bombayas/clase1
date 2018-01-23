08b8ed69e0687ec043fe8ad9c4ed7a22dc7f327c
larriba <33655524+bombayas@users.noreply.github.com>
2018-01-23 11:20:00 +0100

    Add files via upload

---
+++ amigos.agenda.cpp
@@ -0,0 +1,20 @@
+//leer lineasd*/
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    int salir;
+    string mensaje;
+    cout<< dime nº y tlf;
+    cout<< dime nº y tlf;
+    cout<< dime nº y tlf;
+    cout<< dime nº y tlf;
+            cout<<mensaje;
+            cout<<endl;
+      }
+    cin>>salir;
+    
+    
+    }
