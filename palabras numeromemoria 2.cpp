0c3d28fd42f855d369ee9aaba2dac0c467893c0f
larriba <33655524+bombayas@users.noreply.github.com>
2018-02-01 11:13:53 +0100

    Add files via upload

---
+++ palabras numeromemoria 2.cpp
@@ -0,0 +1,11 @@
+//manejo de punteros
+#include<stdio.h>
+
+int main(){
+    char *lista[3];
+    lista[0]=malloc(8*sizeof(char));//memory allocation ye busca hueco para una palabra o varias ****un numero más de el nº de letras de la palabra
+    lista[1]=malloc(7*sizeof(char));
+    lista[2]=malloc(9*sizeof(char));
+    int salir;
+    
+}
