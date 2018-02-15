a9a6d637669a32b6318f60d61dfa1d0db4bc232b
larriba <33655524+bombayas@users.noreply.github.com>
2018-02-15 20:15:59 +0100

    Add files via upload

---
+++ direccioncoches.cpp
@@ -0,0 +1,11 @@
+//programas sobre el manejo de memoria
+#include<stdio.h>
+using namespace std;
+int main(){
+    int a=3;
+    int b=2;
+    int salir;
+    printf("direccion de a= %x\n",&a);//numero entero!!! -- %i  --
+    printf("direccion de b= %x",&b);// cada numero ocupa 4 bits
+    scanf("%c",&salir);//% tanto por ciento ,c caracter
+}
---
+++ gravita.cpp
@@ -0,0 +1,11 @@
+//programas sobre el manejo de memoria
+#include<stdio.h>
+using namespace std;
+int main(){
+    int a=3;
+    int b=2;
+    int salir;
+    printf("direccion de a= %x\n",&a);//numero entero -- %i  --
+    printf("direccion de b= %x",&b);
+    scanf("%c",&salir);
+}
